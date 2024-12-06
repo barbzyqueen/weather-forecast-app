6afe3ca901be48c2c437bc1a123f87a1077a42cf
barbzyqueen <barbaramangiri@gmail.com>
2024-12-06 15:09:44 +0100

    Added forecast HTML from JavaScript

--- src/main.js
+++ src/main.js
@@ -101,3 +101,49 @@ let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Port Harcourt");
+
+
+
+
+
+function displayForecast(){
+    
+    
+    let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+    let forecastHTML ="";
+    
+    days.forEach(function(day){
+    forecastHTML = forecastHTML +
+     `
+                <div class="weather-forecast-day">
+                      <div class="weather-forecast-date">
+                         ${day}
+                      </div>
+                      <div class="weather-forecast-icon">
+                          🌤️
+                      </div>
+
+                      <div class="weather-forecast-temperatures">
+                          <div class="weather-forecast-temperature">
+                              <strong>15 °C </strong>
+                          </div>
+                          <div class="weather-forecast-temperature">
+                              <strong>9 °C </strong>
+                          </div>
+                      </div>
+                  </div>
+
+                                    
+
+     `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+displayForecast();
+
+
+
